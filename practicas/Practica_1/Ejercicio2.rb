36a62e4b98582112741039fa650c0f2a5abfe5ec
Agustin Ortu <ortu.agustin@gmail.com>
2017-09-02 23:07:17 -0300

    Practica 1, Ejercicio 2

---
+++ practicas/Practica_1/Ejercicio2.rb
@@ -0,0 +1,4 @@
+# ¿Qué devuelve la siguiente comparación? ¿Por qué?
+#   'TTPS Ruby'.object_id == 'TTPS Ruby'.object_id
+
+'TTPS Ruby'.object_id == 'TTPS Ruby'.object_id # =>false, porque son dos objetos String distintos, aunque con el mismo contenido
