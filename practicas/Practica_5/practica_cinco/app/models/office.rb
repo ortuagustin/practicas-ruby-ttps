0d422d5d5bcf69d1a6bd4c58f42e0b214a317c7e
Ortu Agustin <ortu.agustin@gmail.com>
2017-12-03 19:51:21 -0300

    la relacion empleado --> oficina puede ser null errores sintacticos en los scopes definidos en los modelos Office y Employee

--- practicas/Practica_5/practica_cinco/app/models/office.rb
+++ practicas/Practica_5/practica_cinco/app/models/office.rb
@@ -1,7 +1,7 @@
 class Office < ApplicationRecord
   has_many :employees # necesario para que funcione el scope que usa employees_count y el counter_cache?
-  scope :available -> { where(available) }
-  scope :empty -> { :available.where(employees_count = 0) }
+  scope :available, -> { where(available: true) }
+  scope :empty, -> { available.where(employees_count: 0) }
 
   def to_s
     "Office named: #{name}, at address: #{address}, with phone: #{phone_number}"
--- practicas/Practica_5/practica_cinco/db/migrate/20171203205116_create_employees.rb
+++ practicas/Practica_5/practica_cinco/db/migrate/20171203205116_create_employees.rb
@@ -3,7 +3,7 @@ class CreateEmployees < ActiveRecord::Migration[5.1]
     create_table :employees do |t|
       t.string :name, null: false, limit: 100
       t.integer :e_number, null: false
-      t.references :office, foreign_key: true
+      t.references :office, foreign_key: true, null: true
 
       t.timestamps
     end
