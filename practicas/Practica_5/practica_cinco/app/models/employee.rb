09e14e3e054b473bcb9c14acbe82d75fafb76891
Ortu Agustin <ortu.agustin@gmail.com>
2017-12-03 20:00:47 -0300

    me agrego esto para consultarlo

--- practicas/Practica_5/practica_cinco/app/models/employee.rb
+++ practicas/Practica_5/practica_cinco/app/models/employee.rb
@@ -2,5 +2,6 @@ class Employee < ApplicationRecord
   belongs_to :office, counter_cache: true, optional: true
   scope :vacant, -> { where(office_id: nil) }
   scope :occupied, -> { where.not(office_id: nil) }
+  scope :occupied_negating_vacant, -> { where.not.vacant } # existe alguna forma de reusar el scope :vacant pero negandolo?
   scope :occupied_association, -> { joins(:office) } # necesito el belongs_to en Employee y el has_many en Office para que me funcione el counter_cache?
 end
